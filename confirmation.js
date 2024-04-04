dfc272184ca48e9045e7c1ffbe9602914e32441a
Joakim Hansen <jocke.hansen@outlook.com>
2024-04-04 16:43:15 +0200

    mvp complete, cart and conf working as intended

--- confirmation.js
+++ confirmation.js
@@ -1,38 +1,105 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Hämta URL-parametrar
-    const urlParams = new URLSearchParams(window.location.search);
-    const name = urlParams.get('name');
-    const phoneNumber = urlParams.get('phoneNumber');
-    const address = urlParams.get('address');
-    const email = urlParams.get('email');
-    const productId = urlParams.get('productId');
-    
-    fetch(`https://fakestoreapi.com/products/${productId}`)
-        .then((response) => response.json())
-        .then((product) => {
-            renderProduct(product);
-        })
-        .catch((error) => {
-            console.error('Error fetching product details:', error);
-        });
+document.addEventListener('DOMContentLoaded', function () {
+  // Hämta URL-parametrar
+  const urlParams = new URLSearchParams(window.location.search)
+  console.log(`${urlParams}`)
+  const fullname = urlParams.get('name')
+  const phoneNumber = urlParams.get('phoneNumber')
+  const address = urlParams.get('address')
+  const email = urlParams.get('email')
 
-    function renderProduct(product) {
-        const card = document.createElement('div');
-        card.classList.add('card');
-                
-        const cardContent = `
-    <img src="${product.image}" class="card-image-top" alt="${product.title}">
-        <div class="card-body">
-            <h6 class="card-title">${product.title}</h6>
-            <p class="card-text">${product.description}</p>
-            <div class="card-button">
-                <h6 class="item-price">${product.price} $</h6>                
-                <a href="form.html?productId=${product.id}">              
-            </div>
-        </div>`
+  function renderCustomerInfo() {
+    const userInfoContainer = document.getElementById('userInfoContainer')
+    const userInfoContent = `
+      <div>    
+          <h2>Delivery adress:</h2>
+          <p>${fullname}</p>
+          <p>${address}</p>
+          <p>${phoneNumber}</p>
+          <p>${email}</p>
+          <hr>
+      </div
+      `
 
-                
-        card.innerHTML = cardContent;
-        document.getElementById('productDisplay').appendChild(card);
+    userInfoContainer.innerHTML = userInfoContent
+  }
+  renderCustomerInfo()
+})
+
+fetch('https://fakestoreapi.com/products')
+  .then((response) => response.json())
+  .then((data) => {
+    let row
+
+    //Get the id's of the products in localstorage
+    let productsInCart = []
+    if (localStorage.getItem('productsInCart') !== null) {
+      productsInCart = JSON.parse(localStorage.getItem('productsInCart'))
     }
-});
+
+    //Filter the products from the API to only render the ones that we have in localstorage
+    const filteredProducts = data.filter((product) => {
+      return productsInCart.some(
+        (cartProduct) => Number(cartProduct.id) === product.id
+      )
+    })
+
+    //Get access to set the total order value text
+    const orderPriceText = document.getElementById('total-price-text')
+    let orderPrice = 0
+
+    //Get the amount of products that have the same id's into an array
+    console.log('productsincart', productsInCart)
+    const idCounts = {}
+    productsInCart.forEach((item) => {
+      const id = item.id
+      idCounts[id] = (idCounts[id] || 0) + 1
+      orderPrice += parseFloat(item.price)
+    })
+
+    //Iterate through the products and render them dynamically
+    filteredProducts.forEach((product, index) => {
+      //Rows with 4 items per row
+      if (index % 4 === 0) {
+        row = document.createElement('div')
+        row.classList.add('row')
+        row.classList.add('product-container')
+        document.getElementById('productDisplay').appendChild(row)
+      }
+
+      //Render the products
+      renderProducts(product, row, idCounts)
+    })
+
+    //Set the total order value text
+    orderPriceText.innerHTML = `${orderPrice.toFixed(2)} $`
+  })
+  .catch((error) => {
+    console.error('Error fetching products:', error)
+  })
+
+function renderProducts(product, row, idCounts) {
+  let counts = idCounts[product.id]
+
+  const col = document.createElement('div')
+
+  const productInCart = document.createElement('div')
+
+  const cardContent = `
+    <div class="container cart-container-1 cart-sizing">
+    <img class="cart-image conf-image"src="${product.image}" alt="${product.title}"
+    <h5 class="cart-title">${product.title}</h5>
+    </div>
+    <div class="container cart-container-2">
+    <div class="cart-price-container">
+        <p class="cart-price">${product.price} $<p>
+    </div>
+    <div class="cart-button-container">        
+        <p>${counts}</p>        
+    </div>
+    <hr>
+    </div>`
+
+  productInCart.innerHTML = cardContent
+  col.appendChild(productInCart)
+  row.appendChild(col)
+}
--- form.js
+++ form.js
@@ -1,31 +1,67 @@
-const urlParams = new URLSearchParams(window.location.search);
-const productId = urlParams.get('productId');
+const urlParams = new URLSearchParams(window.location.search)
+const productId = urlParams.get('productId')
 
-// hämtar den specifika produkten genom produktId
-fetch(`https://fakestoreapi.com/products/${productId}`)
-            .then((response) => response.json())
-            .then((product) => {
-                renderProduct(product);
-            })
-            .catch((error) => {
-                console.error('Error fetching product details:', error);
-            });
+function validateForm() {
+  let formNamn = document.getElementById('name').value
+  let formTelefonNummer = document.getElementById('phoneNumber').value
+  let formAdress = document.getElementById('address').value
+  let formEmail = document.getElementById('email').value
+  let formButton = document.getElementById('submitButton')
 
+  let namn_error = document.getElementById('namn_error')
+  let telefonnr_error = document.getElementById('telefonnr_error')
+  let adress_error = document.getElementById('adress_error')
+  let email_error = document.getElementById('email_error')
 
-// hämtar den specifika produkten genom produktId
-function renderProduct(product) {
-    const card = document.createElement('div');
-    card.classList.add('card');
-            
-    const cardContent = `
-        <img src="${product.image}" class="card-image-top w-25 h-25" alt="${product.title}">
-            <div class="card-body">
-                <h6 class="card-title">${product.title}</h6>
-                <div class="card-button">
-                     <h6 class="item-price">${product.price} $</h6>
-                </div>
-            </div>`;
-            
-    card.innerHTML = cardContent;
-    document.getElementById('productDisplay').appendChild(card);
+  // Denna ifsats låser beställ knappen tills alla fält är korrekt
+  if (formNamn.length < 2) {
+    formButton.disabled = true
+    namn_error.style.display = 'block'
+    namn_error.innerHTML = 'Name is too short'
+    namn_error.style.color = 'red'
+  } else if (formNamn.length > 50) {
+    formButton.disabled = true
+    namn_error.style.display = 'block'
+    namn_error.innerHTML = 'Name is too long'
+  } else if (formTelefonNummer.length > 20) {
+    formButton.disabled = true
+    telefonnr_error.style.display = 'block'
+    telefonnr_error.innerHTML = 'Phonenumber is too long'
+  } else if (formAdress.length < 2) {
+    formButton.disabled = true
+    adress_error.style.display = 'block'
+    adress_error.innerHTML = 'Address too short'
+  } else if (formAdress.length > 50) {
+    formButton.disabled = true
+    adress_error.style.display = 'block'
+    adress_error.innerHTML = 'Address too long'
+  } else if (formEmail.indexOf('@') == -1 || formEmail.length > 50) {
+    formButton.disabled = true
+    email_error.style.display = 'block'
+    email_error.innerHTML = 'Invalid email'
+  } else {
+    formButton.disabled = false
+    namn_error.style.display = 'none'
+    telefonnr_error.style.display = 'none'
+    adress_error.style.display = 'none'
+    email_error.style.display = 'none'
+  }
+
+  //Vi validerar varje fält och ser till att det är ifylld korrekt.
+  if (formNamn.length > 2 && formNamn.length < 50) {
+    namn_error.innerHTML = 'Giltig'
+    namn_error.style.color = 'green'
+  }
+  if (formTelefonNummer.length > 0 && formTelefonNummer.length < 20) {
+    telefonnr_error.innerHTML = 'Giltig'
+    telefonnr_error.style.color = 'green'
+  }
+  if (formAdress.length > 2 && formAdress.length < 50) {
+    adress_error.innerHTML = 'Giltig'
+    adress_error.style.color = 'green'
+  }
+  if (formEmail.indexOf('@') != -1 && formEmail.length < 50) {
+    email_error.innerHTML = 'Giltig'
+    email_error.style.color = 'green'
+  }
 }
